484c940ce4c2f5021c005ef0cf98c8d8e7640f1c
Curlybear <erwanflohic@gmail.com>
2015-10-03 15:40:32 +0000

    Add the async backup job to CB

---
+++ cb/cb_create_job.sql
@@ -0,0 +1,12 @@
+begin
+  dbms_scheduler.create_job (
+    job_name        => 'job_async_backup',
+    job_type        => 'STORED_PROCEDURE',
+    job_action      => 'cb_thing.async_backup',
+    start_date      => SYSTIMESTAMP,
+    repeat_interval => 'FREQ=DAILY;byhour=0; byminute=0; bysecond=0;',
+    auto_drop       => FALSE,
+    comments        => 'async_backup',
+    enabled         => true);
+end;
+/
