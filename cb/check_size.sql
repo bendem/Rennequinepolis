8aab5ae333eb482fb7a04c6c18b181dd65004158
bendem <online@bendem.be>
2015-11-05 23:39:42 +0100

    CS

--- cb/check_size.sql
+++ cb/check_size.sql
@@ -1,17 +1,21 @@
-create or replace function is_number (p_string in varchar2)
-   return int
-is
+create or replace function is_number(
+    p_string in varchar2
+) return int is
    v_new_num number;
 begin
    v_new_num := to_number(p_string);
    return 1;
 exception
-when value_error then
-   return 0;
+    when value_error then
+       return 0;
 end is_number;
 /
 
-create or replace procedure check_size (var in out varchar2, size_var in number, size_max in number) is
+create or replace procedure check_size(
+    var in out varchar2,
+    size_var in number,
+    size_max in number
+) is
 begin
     if length(var) > size_var then
         if length(var) > size_max then
