4c606368b6f0d79532753a1ee4dfad4aab2ca523
Curlybear <erwanflohic@gmail.com>
2015-11-06 02:31:11 +0000

    AlimCB (Multiple insertions) - broken

--- cb/insert_movie.sql
+++ cb/insert_movie.sql
@@ -3,7 +3,7 @@ create or replace type number_t is table of number;
 create or replace type varchar2_t is table of varchar2(4000);
 /
 
-create or replace procedure insert_movie (movie_id in movies.movie_id%type) is
+create or replace procedure insert_movie (v_movie in movies_ext%rowtype) is
 
     split_request constant varchar2(2000) := 'with split(splitted, field, idx) as (
         select
@@ -44,14 +44,14 @@ create or replace procedure insert_movie (movie_id in movies.movie_id%type) is
         where idx < length(field)
     ) select distinct splitted from split';
 
+    raw_data movies_ext%rowtype;
+
     y varchar2(1000);
 
     i pls_integer := 0;
     j pls_integer := 0;
 
     chars1_v varchar2_t;
-
-    raw_data movies_ext%rowtype;
 
     type actors_t                      is table of actors%rowtype index by pls_integer;
     type spoken_languages_t            is table of spoken_languages%rowtype index by pls_integer;
@@ -140,14 +140,14 @@ create or replace procedure insert_movie (movie_id in movies.movie_id%type) is
     -- size_max_genres_name number := 16;
 
 begin
-    select * into raw_data from movies_ext where id = movie_id;
+    raw_data := v_movie;
 
     -- Actors / Characters
     execute immediate split_request bulk collect into chars1_v using raw_data.actors;
 
     for i in chars1_v.first..chars1_v.last loop
-        characters_v(i).movie_id := movie_id;
-        movies_actors_characters_v(i).movie_id := movie_id;
+        characters_v(i).movie_id := raw_data.id;
+        movies_actors_characters_v(i).movie_id := raw_data.id;
 
         j := 1;
         y := regexp_substr(chars1_v(i), '(.+?)(\,{2,}|$)', 1, j);
@@ -178,7 +178,7 @@ begin
     execute immediate split_request bulk collect into chars1_v using raw_data.directors;
 
     for i in chars1_v.first..chars1_v.last loop
-        movies_directors_v(i).movie_id := movie_id;
+        movies_directors_v(i).movie_id := raw_data.id;
 
         j := 1;
         y := regexp_substr(chars1_v(i), '(.+?)(\,{2,}|$)', 1, j);
@@ -203,7 +203,7 @@ begin
     execute immediate split_request bulk collect into chars1_v using raw_data.spoken_languages;
 
     for i in chars1_v.first..chars1_v.last loop
-        movies_spoken_languages_v(i).movie_id := movie_id;
+        movies_spoken_languages_v(i).movie_id := raw_data.id;
 
         j := 1;
         y := regexp_substr(chars1_v(i), '(.+?)(\,{2,}|$)', 1, j);
@@ -226,7 +226,7 @@ begin
     execute immediate split_request bulk collect into chars1_v using raw_data.production_companies;
 
     for i in chars1_v.first..chars1_v.last loop
-        movies_production_companies_v(i).movie_id := movie_id;
+        movies_production_companies_v(i).movie_id := raw_data.id;
 
         j := 1;
         y := regexp_substr(chars1_v(i), '(.+?)(\,{2,}|$)', 1, j);
@@ -249,7 +249,7 @@ begin
     execute immediate split_request bulk collect into chars1_v using raw_data.production_countries;
 
     for i in chars1_v.first..chars1_v.last loop
-        movies_production_countries_v(i).movie_id := movie_id;
+        movies_production_countries_v(i).movie_id := raw_data.id;
 
         j := 1;
         y := regexp_substr(chars1_v(i), '(.+?)(\,{2,}|$)', 1, j);
@@ -272,7 +272,7 @@ begin
     execute immediate split_request bulk collect into chars1_v using raw_data.genres;
 
     for i in chars1_v.first..chars1_v.last loop
-        movies_genres_v(i).movie_id := movie_id;
+        movies_genres_v(i).movie_id := raw_data.id;
 
         j := 1;
         y := regexp_substr(chars1_v(i), '(.+?)(\,{2,}|$)', 1, j);
@@ -290,7 +290,7 @@ begin
         end loop;
     end loop;
 
-    movie_rec.movie_id := movie_id;
+    movie_rec.movie_id := raw_data.id;
     check_size(raw_data.title, size_movies_title , size_max_movies_title);
     movie_rec.movie_title := raw_data.title;
     check_size(raw_data.original_title, size_movies_original_title , size_max_movies_original_title);
@@ -313,8 +313,13 @@ begin
     movie_rec.movie_overview := raw_data.overview;
 
     begin
-        insert into statuses values (null, upper(raw_data.status));
-        insert into certifications values (null, upper(raw_data.certification));
+        if raw_data.status is not null then
+            insert into statuses values (null, upper(raw_data.status));
+        end if;
+
+        if raw_data.certification is not null then
+            insert into certifications values (null, upper(raw_data.certification));
+        end if;
     exception
         when dup_val_on_index then
             null;
@@ -322,107 +327,142 @@ begin
             raise;
     end;
 
-    select status_id into movie_rec.movie_status_id from statuses where upper(status_name) = upper(raw_data.status);
-    select certification_id into movie_rec.movie_certification_id from certifications where upper(certification_name) = upper(raw_data.certification);
+    if raw_data.status is not null then
+        select status_id into movie_rec.movie_status_id from statuses where upper(status_name) = upper(raw_data.status);
+    else
+        select null into movie_rec.movie_status_id from dual;
+    end if;
+
+    if raw_data.certification is not null then
+        select certification_id into movie_rec.movie_certification_id from certifications where upper(certification_name) = upper(raw_data.certification);
+    else
+        select null into movie_rec.movie_certification_id from dual;
+    end if;
+
 
 
     -- INSERTS
+    if actors_v.count <> 0 then
+        for i in actors_v.first .. actors_v.last loop
+            begin
+                insert into actors values actors_v(i);
+            exception
+                when dup_val_on_index then
+                    null;
+                when others then
+                    raise;
+            end;
+        end loop;
+    end if;
 
-    for i in actors_v.first .. actors_v.last loop
-        begin
-            insert into actors values actors_v(i);
-        exception
-            when dup_val_on_index then
-                null;
-            when others then
-                raise;
-        end;
-    end loop;
+    if spoken_languages_v.count <> 0 then
+        for i in spoken_languages_v.first .. spoken_languages_v.last loop
+            begin
+                insert into spoken_languages values spoken_languages_v(i);
+            exception
+                when dup_val_on_index then
+                    null;
+                when others then
+                    raise;
+            end;
+        end loop;
+    end if;
 
-    for i in spoken_languages_v.first .. spoken_languages_v.last loop
-        begin
-            insert into spoken_languages values spoken_languages_v(i);
-        exception
-            when dup_val_on_index then
-                null;
-            when others then
-                raise;
-        end;
-    end loop;
+    if production_countries_v.count <> 0 then
+        for i in production_countries_v.first .. production_countries_v.last loop
+            begin
+                insert into production_countries values production_countries_v(i);
+            exception
+                when dup_val_on_index then
+                    null;
+                when others then
+                    raise;
+            end;
+        end loop;
+    end if;
 
-    for i in production_countries_v.first .. production_countries_v.last loop
-        begin
-            insert into production_countries values production_countries_v(i);
-        exception
-            when dup_val_on_index then
-                null;
-            when others then
-                raise;
-        end;
-    end loop;
+    if production_companies_v.count <> 0 then
+        for i in production_companies_v.first .. production_companies_v.last loop
+            begin
+                insert into production_companies values production_companies_v(i);
+            exception
+                when dup_val_on_index then
+                    null;
+                when others then
+                    raise;
+            end;
+        end loop;
+    end if;
 
-    for i in production_companies_v.first .. production_companies_v.last loop
-        begin
-            insert into production_companies values production_companies_v(i);
-        exception
-            when dup_val_on_index then
-                null;
-            when others then
-                raise;
-        end;
-    end loop;
+    if directors_v.count <> 0 then
+        for i in directors_v.first .. directors_v.last loop
+            begin
+                insert into directors values directors_v(i);
+            exception
+                when dup_val_on_index then
+                    null;
+                when others then
+                    raise;
+            end;
+        end loop;
+    end if;
 
-    for i in directors_v.first .. directors_v.last loop
-        begin
-            insert into directors values directors_v(i);
-        exception
-            when dup_val_on_index then
-                null;
-            when others then
-                raise;
-        end;
-    end loop;
-
-    for i in genres_v.first .. genres_v.last loop
-        begin
-            insert into genres values genres_v(i);
-        exception
-            when dup_val_on_index then
-                null;
-            when others then
-                raise;
-        end;
-    end loop;
+    if genres_v.count <> 0 then
+        for i in genres_v.first .. genres_v.last loop
+            begin
+                insert into genres values genres_v(i);
+            exception
+                when dup_val_on_index then
+                    null;
+                when others then
+                    raise;
+            end;
+        end loop;
+    end if;
 
     insert into movies values movie_rec;
 
-    for i in characters_v.first .. characters_v.last loop
-        insert into characters values characters_v(i);
-    end loop;
+    if characters_v.count <> 0 then
+        for i in characters_v.first .. characters_v.last loop
+            insert into characters values characters_v(i);
+        end loop;
+    end if;
 
-    for i in movies_spoken_languages_v.first .. movies_spoken_languages_v.last loop
-        insert into movies_spoken_languages values movies_spoken_languages_v(i);
-    end loop;
+    if movies_spoken_languages_v.count <> 0 then
+        for i in movies_spoken_languages_v.first .. movies_spoken_languages_v.last loop
+            insert into movies_spoken_languages values movies_spoken_languages_v(i);
+        end loop;
+    end if;
 
-    for i in movies_production_companies_v.first .. movies_production_companies_v.last loop
-        insert into movies_production_companies values movies_production_companies_v(i);
-    end loop;
+    if movies_production_companies_v.count <> 0 then
+        for i in movies_production_companies_v.first .. movies_production_companies_v.last loop
+            insert into movies_production_companies values movies_production_companies_v(i);
+        end loop;
+    end if;
 
-    for i in movies_production_countries_v.first .. movies_production_countries_v.last loop
-        insert into movies_production_countries values movies_production_countries_v(i);
-    end loop;
+    if movies_production_countries_v.count <> 0 then
+        for i in movies_production_countries_v.first .. movies_production_countries_v.last loop
+            insert into movies_production_countries values movies_production_countries_v(i);
+        end loop;
+    end if;
 
-    for i in movies_directors_v.first .. movies_directors_v.last loop
-        insert into movies_directors values movies_directors_v(i);
-    end loop;
+    if movies_directors_v.count <> 0 then
+        for i in movies_directors_v.first .. movies_directors_v.last loop
+            insert into movies_directors values movies_directors_v(i);
+        end loop;
+    end if;
 
-    for i in movies_genres_v.first .. movies_genres_v.last loop
-        insert into movies_genres values movies_genres_v(i);
-    end loop;
+    if movies_genres_v.count <> 0 then
+        for i in movies_genres_v.first .. movies_genres_v.last loop
+            insert into movies_genres values movies_genres_v(i);
+        end loop;
+    end if;
 
-    for i in movies_actors_characters_v.first .. movies_actors_characters_v.last loop
-        insert into movies_actors_characters values movies_actors_characters_v(i);
-    end loop;
+    if movies_actors_characters_v.count <> 0 then
+        for i in movies_actors_characters_v.first .. movies_actors_characters_v.last loop
+            insert into movies_actors_characters values movies_actors_characters_v(i);
+        end loop;
+    end if;
 
     commit;
 exception
---
+++ cb/insert_random_movies.sql
@@ -0,0 +1,37 @@
+create or replace procedure insert_random_movies (n in number) is
+
+    table_record_t is table of movies_ext%rowtype indexed by pls_integer;
+
+    movies table_record_t;
+
+    cursor query is
+        select * from movies_ext order by dbms_random.value;
+begin
+
+    open query;
+
+    for i in 1..n loop
+        fetch query into movies(i);
+    end loop;
+
+    for i in movies.first..movies.last loop
+        insert_movie(movies(i));
+    end loop;
+
+    close query;
+
+exception
+    if query%isopen then
+        close query;
+    end if;
+
+    when others then
+        dbms_output.put_line(sqlerrm);
+        dbms_output.put_line(dbms_utility.format_call_stack);
+        dbms_output.put_line(dbms_utility.format_error_backtrace);
+        rollback;
+        raise;
+end;
+/
+
+exit
