0ef77bc75a6fe44d54d9d5f93e097ffda7308012
bendem <online@bendem.be>
2015-11-14 23:06:38 +0100

    Rework search
    more consistent predicate placements, date -> year, nullcheck comparator

--- cb/search_body.sql
+++ cb/search_body.sql
@@ -1,38 +1,22 @@
 create or replace package body search is
 
-    -- function search(
-    --     p_id  movies.movie_id%type) return sys_refcursor
-    -- is
-    --     x sys_refcursor;
-    -- begin
-    --     open x for select 1 from dual;
-    --     return x;
-    -- end;
-
     function search(
         p_actors varchar2_t default null,
         p_title movies.movie_title%type default null,
         p_directors varchar2_t default null,
-        p_date number default null,
-        p_date_type char default null) return sys_refcursor
+        p_year number default null,
+        p_year_comparison varchar2 default null) return sys_refcursor
     is
         v_parts varchar2_t;
-        v_query varchar2(2000) := 'select * from movies where';
+        v_query varchar2(2000) := 'select * from movies where 1 = 1';
         x sys_refcursor;
     begin
-
         if p_title is not null then
-            v_query := v_query || replace(title_criteria, ':title', p_title);
-        else
-            v_query := v_query || ' 1 = 1 ';
+            v_query := v_query || ' and ' || replace(title_criteria, ':title', p_title);
         end if;
 
-        v_query := v_query || ' and ';
-
-        if p_date is not null then
-            v_query := v_query || replace(replace(date_criteria, ':date', p_date), ':comparator', p_date_type);
-        else
-            v_query := v_query || ' 1 = 1 ';
+        if p_year is not null then
+            v_query := v_query || ' and ' || replace(replace(date_criteria, ':year', p_year), ':comparator', coalesce(p_year_comparison, '='));
         end if;
 
         if p_actors is not null or p_directors is not null then
@@ -46,9 +30,9 @@ create or replace package body search is
                     v_parts := utils.split(p_actors(i), ' ');
 
                     for j in v_parts.first..v_parts.last loop
-                        v_query := v_query || replace(person_part_criteria, ':name', v_parts(j)) || ' and ';
+                        v_query := v_query || ' and ' || replace(person_part_criteria, ':name', v_parts(j));
                     end loop;
-                    v_query := v_query || ' 1 = 1 ';
+
                     if i <> p_actors.last then
                         v_query := v_query || ' intersect ';
                     end if;
@@ -67,9 +51,9 @@ create or replace package body search is
                     v_parts := utils.split(p_directors(i), ' ');
 
                     for j in v_parts.first..v_parts.last loop
-                        v_query := v_query || replace(person_part_criteria, ':name', v_parts(j)) || ' and ';
+                        v_query := v_query || ' and ' || replace(person_part_criteria, ':name', v_parts(j));
                     end loop;
-                    v_query := v_query || ' 1 = 1 ';
+
                     if i <> p_directors.last then
                         v_query := v_query || ' intersect ';
                     end if;
--- cb/search_head.sql
+++ cb/search_head.sql
@@ -1,28 +1,17 @@
 create or replace package search is
 
-    title_criteria constant varchar2(200) := q'[ lower(movie_title) like lower('%:title%') ]';
-
-    date_criteria constant varchar2(200) := q'[ extract(year from movie_release_date) :comparator :date ]';
-
-    actor_base_criteria constant varchar2(200) := q'[ select movie_id from movies natural join movies_actors_characters natural join people where ]';
-
-    director_base_criteria constant varchar2(200) := q'[ select movie_id from movies natural join movies_directors natural join people where ]';
-
-    person_part_criteria constant varchar2(200) := q'[ person_name like '%:name%' ]';
-
-
-
-    -- function search(
-    --     p_id  movies.movie_id%type)
-    -- return sys_refcursor;
-
     function search(
         p_actors varchar2_t default null,
         p_title movies.movie_title%type default null,
         p_directors varchar2_t default null,
-        p_date number default null,
-        p_date_type char default null)
-    return sys_refcursor;
+        p_year number default null,
+        p_year_comparison varchar2 default null) return sys_refcursor;
+
+    title_criteria constant varchar2(200) := q'[lower(movie_title) like lower('%:title%')]';
+    date_criteria constant varchar2(200) := q'[extract(year from movie_release_date) :comparator :year]';
+    actor_base_criteria constant varchar2(200) := q'[select movie_id from movies natural join movies_actors_characters natural join people where 1 = 1]';
+    director_base_criteria constant varchar2(200) := q'[select movie_id from movies natural join movies_directors natural join people where 1 = 1]';
+    person_part_criteria constant varchar2(200) := q'[person_name like '%:name%']';
 
 end search;
 /
