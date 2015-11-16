bb46cb1938483741cd6874776f808ddfbfa4fd90
bendem <online@bendem.be>
2015-11-16 22:43:07 +0100

    Factor search task in a separate class

--- App/src/main/java/be/hepl/benbear/oedapp/SearchController.java
+++ App/src/main/java/be/hepl/benbear/oedapp/SearchController.java
@@ -100,7 +100,11 @@ public class SearchController implements Initializable {
 
         if(task != null && task.isRunning()) {
             task.cancel();
-            while(task.isRunning());
+            while(task.isRunning()) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch(InterruptedException e) {}
+            }
         }
 
         Map<String, List<String>> query = parser.parse(text);
@@ -109,41 +113,7 @@ public class SearchController implements Initializable {
         count = 0;
         updateCount();
 
-        task = new Task<Movie>() {
-            @Override
-            protected Movie call() throws Exception {
-                try(CallableStatement cs = buildQuery(app.getConnection(), query)) {
-                    cs.execute();
-                    try(ResultSet rs = (ResultSet) cs.getObject(1)) {
-
-                        while(rs.next()) {
-                            if(isCancelled()) {
-                                break;
-                            }
-
-                            updateValue(new Movie(
-                                ResultSetExtractor.getInt(rs, "movie_id").getAsInt(),
-                                ResultSetExtractor.getString(rs, "movie_title").get(),
-                                ResultSetExtractor.getString(rs, "movie_original_title").get(),
-                                ResultSetExtractor.getDate(rs, "movie_release_date").map(Date::toLocalDate).orElse(null),
-                                ResultSetExtractor.getString(rs, "status_name").orElse("(empty)"),
-                                ResultSetExtractor.getDouble(rs, "movie_vote_avg").getAsDouble(),
-                                ResultSetExtractor.getInt(rs, "movie_vote_count").getAsInt(),
-                                ResultSetExtractor.getInt(rs, "movie_runtime").orElse(0),
-                                ResultSetExtractor.getBytes(rs, "image").orElseGet(app::getEmptyImage),
-                                ResultSetExtractor.getInt(rs, "movie_budget").getAsInt(),
-                                ResultSetExtractor.getInt(rs, "movie_revenue").getAsInt(),
-                                ResultSetExtractor.getString(rs, "movie_homepage").orElse("(empty)"),
-                                ResultSetExtractor.getString(rs, "movie_tagline").orElse("(empty)"),
-                                ResultSetExtractor.getString(rs, "movie_overview").orElse("(empty)")
-                            ));
-                            ++count;
-                        }
-                        return null;
-                    }
-                }
-            }
-        };
+        task = new SearchTask(query);
         task.valueProperty().addListener((obs, o, n) -> {
             if(n == null) {
                 return;
@@ -174,95 +144,6 @@ public class SearchController implements Initializable {
         countText.setText(String.valueOf(count) + " result" + (count > 1 ? 's' : ""));
     }
 
-    private CallableStatement buildQuery(Connection connection, Map<String, List<String>> query) throws SQLException {
-        if(query.containsKey("id")) {
-            CallableStatement stmt = connection.prepareCall("{ ? = call search.search(p_id => ?) }");
-            stmt.registerOutParameter(1, OracleTypes.CURSOR);
-            stmt.setInt(2, Integer.parseInt(query.get("id").get(0)));
-            return stmt;
-        }
-
-        List<String> title = query.getOrDefault("title", Collections.emptyList());
-        List<String> actors = query.getOrDefault("actor", Collections.emptyList());
-        List<String> directors = query.getOrDefault("director", Collections.emptyList());
-        List<String> before = query.getOrDefault("before", Collections.emptyList());
-        List<String> after = query.getOrDefault("after", Collections.emptyList());
-        List<String> during = query.getOrDefault("during", Collections.emptyList());
-
-        // Generate query
-        StringBuilder sb = new StringBuilder("{ ? = call search.search(");
-        if(!title.isEmpty()) {
-            sb.append("p_title => ?, ");
-        }
-        if(!actors.isEmpty()) {
-            sb
-                .append("p_actors => varchar2_t(")
-                .append(generatePlaceholders(query.get("actor").size()))
-                .append("), ");
-        }
-        if(!directors.isEmpty()) {
-            sb
-                .append("p_directors => varchar2_t(")
-                .append(generatePlaceholders(query.get("director").size()))
-                .append("), ");
-        }
-        if(!before.isEmpty() || !after.isEmpty() || !during.isEmpty()) {
-            int count = before.size() + after.size() + during.size();
-            sb
-                .append("p_years => number_t(")
-                .append(generatePlaceholders(count))
-                .append("), ")
-                .append("p_years_comparisons => varchar2_t(")
-                .append(generatePlaceholders(count))
-                .append("), ");
-        }
-        sb.setLength(sb.length() - 2); // Removes trailing ", "
-        sb.append(") }");
-        System.out.println(sb);
-
-        CallableStatement cs = connection.prepareCall(sb.toString());
-
-        // Bind stuff
-        int i = 0;
-        cs.registerOutParameter(++i, OracleTypes.CURSOR);
-        if(!title.isEmpty()) {
-            cs.setString(++i, String.join(" ", title));
-        }
-        if(!actors.isEmpty()) {
-            for(String actor : actors) {
-                cs.setString(++i, actor);
-            }
-        }
-        if(!directors.isEmpty()) {
-            for(String director : directors) {
-                cs.setString(++i, director);
-            }
-        }
-        if(!before.isEmpty() || !after.isEmpty() || !during.isEmpty()) {
-            for(String bef : before) {
-                cs.setInt(++i, Integer.parseInt(bef));
-            }
-            for(String aft : after) {
-                cs.setInt(++i, Integer.parseInt(aft));
-            }
-            for(String dur : during) {
-                cs.setInt(++i, Integer.parseInt(dur));
-            }
-
-            for(String s : before) {
-                cs.setString(++i, "<");
-            }
-            for(String s : after) {
-                cs.setString(++i, ">");
-            }
-            for(String s : during) {
-                cs.setString(++i, "=");
-            }
-        }
-
-        return cs;
-    }
-
     private String generatePlaceholders(int count) {
         return IntStream.range(0, count).mapToObj(i -> "?").collect(Collectors.joining(", "));
     }
@@ -274,4 +155,138 @@ public class SearchController implements Initializable {
         dialog.getDialogPane().getButtonTypes().add(new ButtonType("OK", ButtonBar.ButtonData.OK_DONE));
         dialog.show();
     }
+
+    private class SearchTask extends Task<Movie> {
+
+        private final Map<String, List<String>> query;
+
+        public SearchTask(Map<String, List<String>> query) {
+            this.query = query;
+        }
+
+        @Override
+        protected Movie call() throws Exception {
+            try(CallableStatement cs = buildQuery(app.getConnection(), query)) {
+                cs.execute();
+
+                try(ResultSet rs = (ResultSet) cs.getObject(1)) {
+                    while(rs.next()) {
+                        if(isCancelled()) {
+                            break;
+                        }
+
+                        updateValue(new Movie(
+                            ResultSetExtractor.getInt(rs, "movie_id").getAsInt(),
+                            ResultSetExtractor.getString(rs, "movie_title").get(),
+                            ResultSetExtractor.getString(rs, "movie_original_title").get(),
+                            ResultSetExtractor.getDate(rs, "movie_release_date").map(Date::toLocalDate).orElse(null),
+                            ResultSetExtractor.getString(rs, "status_name").orElse("(empty)"),
+                            ResultSetExtractor.getDouble(rs, "movie_vote_avg").getAsDouble(),
+                            ResultSetExtractor.getInt(rs, "movie_vote_count").getAsInt(),
+                            ResultSetExtractor.getInt(rs, "movie_runtime").orElse(0),
+                            ResultSetExtractor.getBytes(rs, "image").orElseGet(app::getEmptyImage),
+                            ResultSetExtractor.getInt(rs, "movie_budget").getAsInt(),
+                            ResultSetExtractor.getInt(rs, "movie_revenue").getAsInt(),
+                            ResultSetExtractor.getString(rs, "movie_homepage").orElse("(empty)"),
+                            ResultSetExtractor.getString(rs, "movie_tagline").orElse("(empty)"),
+                            ResultSetExtractor.getString(rs, "movie_overview").orElse("(empty)")
+                        ));
+                        ++count;
+                    }
+                    return null;
+                }
+            }
+        }
+
+        private CallableStatement buildQuery(Connection connection, Map<String, List<String>> query) throws SQLException {
+            if(query.containsKey("id")) {
+                CallableStatement stmt = connection.prepareCall("{ ? = call search.search(p_id => ?) }");
+                stmt.registerOutParameter(1, OracleTypes.CURSOR);
+                stmt.setInt(2, Integer.parseInt(query.get("id").get(0)));
+                return stmt;
+            }
+
+            List<String> title = query.getOrDefault("title", Collections.emptyList());
+            List<String> actors = query.getOrDefault("actor", Collections.emptyList());
+            List<String> directors = query.getOrDefault("director", Collections.emptyList());
+            List<String> before = query.getOrDefault("before", Collections.emptyList());
+            List<String> after = query.getOrDefault("after", Collections.emptyList());
+            List<String> during = query.getOrDefault("during", Collections.emptyList());
+
+            // Generate query
+            StringBuilder sb = new StringBuilder("{ ? = call search.search(");
+            if(!title.isEmpty()) {
+                sb.append("p_title => ?, ");
+            }
+            if(!actors.isEmpty()) {
+                sb
+                    .append("p_actors => varchar2_t(")
+                    .append(generatePlaceholders(query.get("actor").size()))
+                    .append("), ");
+            }
+            if(!directors.isEmpty()) {
+                sb
+                    .append("p_directors => varchar2_t(")
+                    .append(generatePlaceholders(query.get("director").size()))
+                    .append("), ");
+            }
+            if(!before.isEmpty() || !after.isEmpty() || !during.isEmpty()) {
+                int count = before.size() + after.size() + during.size();
+                sb
+                    .append("p_years => number_t(")
+                    .append(generatePlaceholders(count))
+                    .append("), ")
+                    .append("p_years_comparisons => varchar2_t(")
+                    .append(generatePlaceholders(count))
+                    .append("), ");
+            }
+            sb.setLength(sb.length() - 2); // Removes trailing ", "
+            sb.append(") }");
+            System.out.println(sb);
+
+            CallableStatement cs = connection.prepareCall(sb.toString());
+
+            // Bind stuff
+            int i = 0;
+            cs.registerOutParameter(++i, OracleTypes.CURSOR);
+            if(!title.isEmpty()) {
+                cs.setString(++i, String.join(" ", title));
+            }
+            if(!actors.isEmpty()) {
+                for(String actor : actors) {
+                    cs.setString(++i, actor);
+                }
+            }
+            if(!directors.isEmpty()) {
+                for(String director : directors) {
+                    cs.setString(++i, director);
+                }
+            }
+            if(!before.isEmpty() || !after.isEmpty() || !during.isEmpty()) {
+                for(String bef : before) {
+                    cs.setInt(++i, Integer.parseInt(bef));
+                }
+                for(String aft : after) {
+                    cs.setInt(++i, Integer.parseInt(aft));
+                }
+                for(String dur : during) {
+                    cs.setInt(++i, Integer.parseInt(dur));
+                }
+
+                for(String s : before) {
+                    cs.setString(++i, "<");
+                }
+                for(String s : after) {
+                    cs.setString(++i, ">");
+                }
+                for(String s : during) {
+                    cs.setString(++i, "=");
+                }
+            }
+
+            return cs;
+        }
+
+    }
+
 }
