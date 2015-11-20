8110c4aed70cc456f19db92d0f0a21250b31226e
bendem <online@bendem.be>
2015-11-20 21:48:28 +0100

    Add debug procedures

--- utils/utils_head.sql
+++ utils/utils_head.sql
@@ -8,14 +8,14 @@ create or replace package utils is
     -- @param p_replacement the value replacing the variable if its length is greater
     --                      than p_size_max
     procedure check_size(
-        p_var in out varchar2,
+        p_var in out nocopy varchar2,
         p_size_var in pls_integer,
         p_size_max in pls_integer,
         p_replacement in varchar2 default 'undefined');
 
     -- See above
     procedure check_size(
-        p_var in out number,
+        p_var in out nocopy number,
         p_size_var in pls_integer,
         p_size_max in pls_integer,
         p_replacement in number default 0);
@@ -26,6 +26,22 @@ create or replace package utils is
     function split(
         p_string in varchar2,
         p_separator in varchar2) return varchar2_t;
+
+    -- Prints the content of a nested table to the dbms_output.
+    --
+    -- Note that the table is passed in out nocopy for performance
+    -- reasons but is not modified.
+    -- @param p_table  the table to display
+    procedure debug(
+        p_table in out nocopy varchar2_t);
+
+    -- Prints the content of a nested table to the dbms_output.
+    --
+    -- Note that the table is passed in out nocopy for performance
+    -- reasons but is not modified.
+    -- @param p_table  the table to display
+    procedure debug(
+        p_table in out nocopy number_t);
 
 end utils;
 /
