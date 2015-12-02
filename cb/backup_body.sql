8cc791f05476e1f6d2db8b61f4f2bd43111e747e
bendem <online@bendem.be>
2015-12-02 17:53:22 +0100

    Brace yourselves, the great formatter overlord is coming!

--- cb/backup_body.sql
+++ cb/backup_body.sql
@@ -104,109 +104,130 @@ create or replace package body backup is
         Logging.i('Transfering Images');
         merge into images@link.backup there using (
             select * from images where image_id in (
-                select movie_poster_id from movies where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+                select movie_poster_id from movies where movie_id in (
+                    select movie_id from movies where backup_flag = 0
+                )
                 union
                 select person_profile_id from people where person_id in (
-                    select person_id from movies_directors where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+                    select person_id from movies_directors where movie_id in (
+                        select movie_id from movies where backup_flag = 0
+                    )
                     union
-                    select person_id from characters where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+                    select person_id from characters where movie_id in (
+                        select movie_id from movies where backup_flag = 0
+                    )
                 )
             )
         ) here on (there.image_id = here.image_id)
-            when not matched then
-                insert values (
-                    here.image_id,
-                    here.image_path,
-                    here.image
-        );
+        when not matched then
+            insert values (
+                here.image_id,
+                here.image_path,
+                here.image
+            );
 
         Logging.i('Transfering people');
         merge into people@link.backup there using (
             select * from people where person_id in (
-                select person_id from movies_directors where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+                select person_id from movies_directors where movie_id in (
+                    select movie_id from movies where backup_flag = 0
+                )
                 union
-                select person_id from characters where movie_id in (select movie_id bulk from movies where backup_flag = 0))
+                select person_id from characters where movie_id in (
+                    select movie_id from movies where backup_flag = 0
+                )
+            )
         ) here on (there.person_id = here.person_id)
-            when not matched then
-                insert values (
-                    here.person_id,
-                    here.person_name,
-                    here.person_profile_id
-        );
+        when not matched then
+            insert values (
+                here.person_id,
+                here.person_name,
+                here.person_profile_id
+            );
 
         Logging.i('Transfering genres');
         merge into genres@link.backup there using (
             select * from genres where genre_id in (
-                select genre_id from movies_genres where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+                select genre_id from movies_genres where movie_id in (
+                    select movie_id from movies where backup_flag = 0
+                )
             )
         ) here on (there.genre_id = here.genre_id)
-            when not matched then
-                insert values (
-                    here.genre_id,
-                    here.genre_name
-        );
+        when not matched then
+            insert values (
+                here.genre_id,
+                here.genre_name
+            );
 
         Logging.i('Transfering certifications');
         merge into certifications@link.backup there using (
             select * from certifications where certification_id in (
-                select certification_id from movies where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+                select certification_id from movies where movie_id in (
+                    select movie_id from movies where backup_flag = 0
+                )
             )
         ) here on (there.certification_id = here.certification_id)
-            when not matched then
-                insert values (
-                    here.certification_id,
-                    here.certification_name
-        );
+        when not matched then
+            insert values (
+                here.certification_id,
+                here.certification_name
+            );
 
         Logging.i('Transfering statuses');
         merge into statuses@link.backup there using (
             select * from statuses where status_id in (
-                select status_id from movies where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+                select status_id from movies where movie_id in (
+                    select movie_id from movies where backup_flag = 0
+                )
             )
         ) here on (there.status_id = here.status_id)
-            when not matched then
-                insert values (
-                    here.status_id,
-                    here.status_name
-        );
+        when not matched then
+            insert values (
+                here.status_id,
+                here.status_name
+            );
 
         Logging.i('Transfering spoken_languages');
         merge into spoken_languages@link.backup there using (
             select * from spoken_languages where spoken_language_id in (
-                select spoken_language_id from movies_spoken_languages where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+                select spoken_language_id from movies_spoken_languages where movie_id in (
+                    select movie_id from movies where backup_flag = 0
+                )
             )
         ) here on (there.spoken_language_id = here.spoken_language_id)
-            when not matched then
-                insert values (
-                    here.spoken_language_id,
-                    here.spoken_language_name
-        );
+        when not matched then
+            insert values (
+                here.spoken_language_id,
+                here.spoken_language_name
+            );
 
         Logging.i('Transfering production_countries');
         merge into production_countries@link.backup there using (
             select * from production_countries where production_country_id in (
-                select production_country_id from movies_production_countries where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+                select production_country_id from movies_production_countries where movie_id in (
+                    select movie_id from movies where backup_flag = 0
+                )
             )
         ) here on (there.production_country_id = here.production_country_id)
-            when not matched then
-                insert values (
-                    here.production_country_id,
-                    here.production_country_name
-        );
+        when not matched then
+            insert values (
+                here.production_country_id,
+                here.production_country_name
+            );
 
         Logging.i('Transfering production_companies');
         merge into production_companies@link.backup there using (
             select * from production_companies where production_company_id in (
-                select production_company_id from movies_production_companies where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+                select production_company_id from movies_production_companies where movie_id in (
+                    select movie_id from movies where backup_flag = 0
+                )
             )
         ) here on (there.production_company_id = here.production_company_id)
-            when not matched then
-                insert values (
-                    here.production_company_id,
-                    here.production_company_name
-        );
-
-
+        when not matched then
+            insert values (
+                here.production_company_id,
+                here.production_company_name
+            );
 
         Logging.i('Transfering movies');
         merge into movies@link.backup there using (
@@ -250,51 +271,61 @@ create or replace package body backup is
                 here.movie_overview,
                 here.movie_copies,
                 1
-        );
+            );
 
         Logging.i('Transfering movies_production_countries');
         merge into movies_production_countries@link.backup there using (
-            select * from movies_production_countries where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+            select * from movies_production_countries where movie_id in (
+                select movie_id from movies where backup_flag = 0
+            )
         ) here on (there.movie_id = here.movie_id and there.production_country_id = here.production_country_id)
         when not matched then
             insert values (
                 here.movie_id,
                 here.production_country_id
-        );
+            );
 
         Logging.i('Transfering movies_production_companies');
         merge into movies_production_companies@link.backup there using (
-            select * from movies_production_companies where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+            select * from movies_production_companies where movie_id in (
+                select movie_id from movies where backup_flag = 0
+            )
         ) here on (there.movie_id = here.movie_id and there.production_company_id = here.production_company_id)
         when not matched then
             insert values (
                 here.movie_id,
                 here.production_company_id
-        );
+            );
 
         Logging.i('Transfering movies_directors');
         merge into movies_directors@link.backup there using (
-            select * from movies_directors where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+            select * from movies_directors where movie_id in (
+                select movie_id from movies where backup_flag = 0
+            )
         ) here on (there.movie_id = here.movie_id and there.person_id = here.person_id)
         when not matched then
             insert values (
                 here.movie_id,
                 here.person_id
-        );
+            );
 
         Logging.i('Transfering movies_genres');
         merge into movies_genres@link.backup there using (
-            select * from movies_genres where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+            select * from movies_genres where movie_id in (
+                select movie_id from movies where backup_flag = 0
+            )
         ) here on (there.movie_id = here.movie_id and there.genre_id = here.genre_id)
         when not matched then
             insert values (
                 here.movie_id,
                 here.genre_id
-        );
+            );
 
         Logging.i('Transfering characters');
         merge into characters@link.backup there using (
-            select * from characters where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+            select * from characters where movie_id in (
+                select movie_id from movies where backup_flag = 0
+            )
         ) here on (there.movie_id = here.movie_id and there.character_id = here.character_id)
         when not matched then
             insert values (
@@ -302,22 +333,23 @@ create or replace package body backup is
                 here.character_id,
                 here.person_id,
                 here.character_name
-        );
+            );
 
         Logging.i('Transfering copies');
         merge into copies@link.backup there using (
-            select * from copies where movie_id in (select movie_id bulk from movies where backup_flag = 0)
+            select * from copies where movie_id in (
+                select movie_id from movies where backup_flag = 0
+            )
         ) here on (there.movie_id = here.movie_id)
         when not matched then
             insert values (
                 here.movie_id,
                 here.copy_id,
                 1
-        );
+            );
 
         update movies set backup_flag = 1 where backup_flag = 0;
         update copies set backup_flag = 1 where backup_flag = 0;
-
     end;
 
 end backup;
