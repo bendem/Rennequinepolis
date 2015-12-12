dd6c4141a2db006fb3ddb842385cbc2f45268d40
Curlybear <Tehcurlybear@gmail.com>
2015-12-12 16:50:00 +0100

    Move copies out of the movies

--- cc/create_table.sql
+++ cc/create_table.sql
@@ -23,12 +23,24 @@ varray "XMLDATA"."review" store as table review_table (
 varray "XMLDATA"."spoken_language" store as table spoken_language_table (
     (primary key (nested_table_id, sys_nc_array_index$))
 )
-varray "XMLDATA"."copy" store as table copy_table (
+;
+
+create table copies of xmltype
+xmltype store as object relational
+xmlschema "http://xmlns.bendem.be/cc"
+element "copy";
+
+create table schedules of xmltype
+xmltype store as object relational
+xmlschema "http://xmlns.bendem.be/cc"
+element "schedule"
+varray "XMLDATA"."time_schedule" store as table time_schedule_table (
     (primary key (nested_table_id, sys_nc_array_index$))
 )
---  TODO FIX ME (Can't access child of child somehow)
---  varray "XMLDATA"."copy"."schedule"
---    store as table schedule_table ((primary key (nested_table_id, sys_nc_array_index$)))
 ;
 
 alter table movies add constraint pk_movies primary key (XMLDATA."id");
+alter table copies add constraint pk_copies primary key (XMLDATA."copy_id", XMLDATA."movie_id");
+alter table schedules add constraint pk_schedules primary key (XMLDATA."copy_id", XMLDATA."movie_id");
+alter table copies add constraint fk_copies foreign key (XMLDATA."movie_id") references movies(XMLDATA."id");
+alter table schedules add constraint fk_schedules foreign key (XMLDATA."movie_id") references movies(XMLDATA."id");
